fbccc9fb9636dc02419006799b43b5c42bb6b972
pedroferens <169068429+pedroferens@users.noreply.github.com>
2024-05-13 14:39:01 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+ public class Main {
+  public static void main(String[] args) {
+    system.out.println("#########");
+    System.out.println("# mc ph #");
+    system.out.println("#########");
+
+    system.out.println("");
+
+    system.out.println("album: O Cara Do Momento, Vol 2");
+    system.out.println("Ano De Lançamento: 2024");
+
+    system.out.println("");
+
+    system.out.println("info");
+
+    system.out.println("");
+
+    system.out.println("++++++++")
+    system.out.println("+musica+")
+    system.out.println("++++++++")
+  }
+
+  
+  
+  
+  
+}
